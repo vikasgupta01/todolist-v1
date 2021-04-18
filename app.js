1c70153e86f9003ae0ced6c15338c5cf8ddc6d73
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-18 14:53:46 +0530

    Remove lecture specific comments for 3/22 EJS

--- app.js
+++ app.js
@@ -8,22 +8,11 @@ const app = express();
 
 // simple get route that just sends the browser word Hello when a user tries to access the home route
 app.get("/", function (req, res) {
-    // res.send("Hello"); // We pass data from our server (app.js) to the browser via this send method. 
-    // This allows us to perform logic/do processing on our server side and send only response to the browser.
-    // This also allows render different results to users depending on the logic that we include inside our server.
-    // res.send only allows us to send one piece of data, as the server considers it final sending instruction. To send multiple pieces of data, use res.write.
-    // res.send is like sending messages through messenger, while res.write is like doing so from email.
 
     // let's check if today is a weekend or not
     var today = new Date();
     if (today.getDay() === 6 || today.getDay() === 0) {
-        // res.write("<h1>It's the weekend!!</h1>");
-        // res.write("<p>Yay, I'm gonna code more, and keep myself from being miserable...</p>")
-        // res.send();
-
         res.sendFile(__dirname + "/index.html");
-        // now creating a different file for each day won't be less of a pain. So to tackle this we use html templating (where we can change certain parts of the  
-        // html, depending on the logic on our server).
     } else {
         res.write("<h1>It's a working day!!</h1>");
         res.write("<p>Yay, I'm gonna watch more content and work less...</p>")
