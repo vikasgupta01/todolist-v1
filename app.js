8f1a138d6e68a474f17ce91af2cf11ef41e42c91
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-18 16:20:49 +0530

    Commit for - 4/22 EJS:Creating first ejs templates

--- app.js
+++ app.js
@@ -6,18 +6,60 @@ const bodyParser = require("body-parser");
 // creating app const using express
 const app = express();
 
+// visit ejs general documentation {https://ejs.co/} & documentation on how to use it with express. {https://github.com/mde/ejs/wiki/Using-EJS-with-Express}
+// install ejs in project directory using command 'npm install ejs' in terminal
+// check for ejs dependency in package.json file.
+// create a 'views' folder in project where you'll keep all the views.
+// make sure to put this after declaring app. Otherwise you'll be using app before initializing it and will get error.
+app.set('view engine', 'ejs');
+
 // simple get route that just sends the browser word Hello when a user tries to access the home route
 app.get("/", function (req, res) {
 
     // let's check if today is a weekend or not
     var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.sendFile(__dirname + "/index.html");
-    } else {
-        res.write("<h1>It's a working day!!</h1>");
-        res.write("<p>Yay, I'm gonna watch more content and work less...</p>")
-        res.send();
+    var currentDay = today.getDay();
+    var day = "";
+
+    // if (currentDay === 6 || currentDay === 0) {
+    //     day = "Weekend";
+    // } else {
+    //     day = "Weekday";
+    // }
+
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            day = "Default triggered, which means our code broke. Check console and app.js file";
+            console.log("Error : Current day is equal to : " + currentDay);
+            break;
     }
+
+
+    // we'll use res.render method in app.js instead of res.send or res.sendFile (see https://github.com/mde/ejs/wiki/Using-EJS-with-Express)
+    res.render("list", { kindOfDay: day });
+    // kindOfDay variable is used for clarity on variable assignment. Usually, we'll use name 'day' for both.
 });
 
 
