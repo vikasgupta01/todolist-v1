ca15a82ba34e896e2673ed0a30a4aa757ad092f4
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-19 03:47:52 +0530

    Add code for 6/22-EJS : Passing Data from your webpage to your server

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// Requiring the two packages that we just installed
+// 6/22-EJS : Passing data from your webpage to your server
+// Fundamentals of templating explaining passing data from server to template and to server from template.
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -6,46 +8,47 @@ const bodyParser = require("body-parser");
 // creating app const using express
 const app = express();
 
+// need to define item here to increase it's scope and be able to use in app.get as well as in app.post
+var items = ["Buy Food", "Cook Food", "Eat Food", "Shit Food"];
+// made it a collection to be able to store multiple inputs
+
 // make sure to put this after declaring app. Otherwise you'll be using app before initializing it and will get error.
 app.set('view engine', 'ejs');
 
-// simple get route that just sends the browser word Hello when a user tries to access the home route
+// telling our app to use bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// 1. HomePage tha gets loaded first
 app.get("/", function (req, res) {
 
     // let's check if today is a weekend or not
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            day = "Default triggered, which means our code broke. Check console and app.js file";
-            console.log("Error : Current day is equal to : " + currentDay);
-            break;
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    res.render("list", { kindOfDay: day });
+    var day = today.toLocaleDateString("en-US", options);
+
+    //2. after passing through steps above, we render list.ejs passing in two variables (first contains date info, and second has items array) which get read there.
+    res.render("list", { kindOfDay: day, newListItems: items });
+    // day gets passed over to list.ejs as a variable kindOfDay
+});
+
+app.post("/", function (req, res) {
+    // 5. when we're inside this block of code, we grab hold of value of newItem (entered through UI input), we save it to a variable 'item'
+    var item = req.body.newItem;
+
+    // and we add that item to our array items
+    items.push(item);
+    // res.render("list", { newListItem: item });
+
+    // and then we redirect to the home route. Taking us back to app.get method, with added element in items array.
+    res.redirect("/");
+    // 6. now we're able to render list again, and pass over now updated array to the list.ejs
 });
 
 
